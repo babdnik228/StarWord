457799c065567386e3fee26c91ba11f5a9047c0c
alexgodi <103599099+babdnik228@users.noreply.github.com>
2022-04-23 02:00:14 +0300

    Update 1.17

--- Assets/_Game/OneCastScene/InputClickToSceme.cs
+++ Assets/_Game/OneCastScene/InputClickToSceme.cs
@@ -7,14 +7,37 @@ public class InputClickToSceme : MonoBehaviour
 {
     [SerializeField]
     private int _nameScene;
+    [SerializeField]
+    private GameObject _buttonIcon;
+    [SerializeField]
+    private GameObject _animation;
     private void OnTriggerStay(Collider other)
     {
         PlayerManager character = other.GetComponent<PlayerManager>();
         if(character != null)
         {
-            
-                SceneManager.LoadScene(_nameScene);
-            
+            _buttonIcon.SetActive(true);
+            if(character && Input.GetKeyDown(KeyCode.E))
+            {
+                StartCoroutine(Cor());
+            }
         }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        PlayerManager character = other.GetComponent<PlayerManager>();
+        if (character != null)
+        {
+            _buttonIcon.SetActive(false);
+        }
+    }
+    private IEnumerator Cor()
+    {
+        yield return new WaitForSeconds(1);
+        _buttonIcon.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        _animation.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(_nameScene);
     }
 }
---
+++ Assets/_Game/OneCastScene/SwitchClick.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchClick : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _eRestartButton;
+    [SerializeField]
+    private GameObject _fToGame;
+    private void OnTriggerStay(Collider other)
+    {
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+}
