7a4174f271ebbaaeb6e7fcb452ee8a3d0f75538f
alexgodi <103599099+babdnik228@users.noreply.github.com>
2022-05-03 12:49:54 +0300

    Update 1.2

--- Assets/_GameSolo/Character/CharacterMaster.cs
+++ Assets/_GameSolo/Character/CharacterMaster.cs
@@ -4,7 +4,44 @@ using UnityEngine;
 
 public class CharacterMaster : MonoBehaviour
 {
+    [SerializeField] private float speedNormal;
+    [SerializeField] private float jumpForce;
 
- 
+    private float gravity;
+    private Vector3 moveVec;
+
+    private CharacterController character;
+    private Animator anim;
+
+    private void Awake()
+    {
+        character = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        CharacterMoveLogic();
+        CharacterGravityLogic();
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+    private void CharacterMoveLogic()
+    {
+        moveVec = Vector3.zero;
+        moveVec.x = Input.GetAxisRaw("Horizontal") * speedNormal;
+        moveVec.z = Input.GetAxisRaw("Vertical") * speedNormal;
+        moveVec.y = gravity;
+
+        character.Move(moveVec * Time.deltaTime);
+    }
+    private void CharacterGravityLogic()
+    {
+        if (!character.isGrounded) gravity -= 20 * Time.deltaTime;
+        else gravity = -1f;
+    }
+
+
 
 }
