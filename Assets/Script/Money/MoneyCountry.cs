87c5fe34c66ba14b8194a2ca1fe8e6b1ece80ca7
alexgodi <103599099+babdnik228@users.noreply.github.com>
2022-07-06 00:36:20 +0300

    Economic Transit

--- Assets/Script/Money/MoneyCountry.cs
+++ Assets/Script/Money/MoneyCountry.cs
@@ -6,6 +6,8 @@ public class MoneyCountry : MonoBehaviour
 {
     [SerializeField] private ParametrsCountry _parCountry;
     public int coins_c;
+
+   
     private void FixedUpdate()
     {
         _parCountry.coins_country+=coins_c;
---
+++ Assets/Script/Money/MoneyTarget/ManipMoney.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManipMoney : MonoBehaviour
+{
+    [SerializeField] private MoneyCountry moneyCountry;
+    [SerializeField] private ParametrsCountry parametrsCountry;
+    //Money
+    public void BuyCountryMoney()
+    {
+        if(parametrsCountry.coins_country >= 25)
+        {
+        parametrsCountry.coins_country -= 25;
+        parametrsCountry.coins_word += 5;
+        }
+    }
+    public void BuyWordCountryMoney()
+    {
+        if(parametrsCountry.coins_word >= 6)
+        {
+        parametrsCountry.coins_country += 20;
+        parametrsCountry.coins_word -= 6;
+        }
+    }
+    //Builder
+    public void ByeScholl()
+    {
+        if(parametrsCountry.scholl >= 1)
+        {
+            parametrsCountry.scholl -= 1;
+            parametrsCountry.coins_country += 75;
+        }
+    }
+    public void ByeFabric()
+    {
+        if (parametrsCountry.fabric >= 1)
+        {
+            parametrsCountry.fabric -= 1;
+            parametrsCountry.coins_country += 75;
+        }
+    }
+    public void ByeTheatr()
+    {
+        if (parametrsCountry.teatr >= 1)
+        {
+            parametrsCountry.teatr -= 1;
+            parametrsCountry.coins_country += 75;
+        }
+    }
+    //Specialist
+    public void ByeSpecialist()
+    {
+        if (parametrsCountry.specialists >= 100)
+        {
+            parametrsCountry.specialists -= 100;
+            parametrsCountry.coins_country += 50;
+        }
+    }
+    public void ByeMaterials()
+    {
+        if (parametrsCountry.specialists >= 100)
+        {
+            parametrsCountry.specialists -= 100;
+            parametrsCountry.coins_country += 50;
+        }
+    }
+    public void ByeMovie()
+    {
+        if (parametrsCountry.movies >= 100)
+        {
+            parametrsCountry.movies -= 100;
+            parametrsCountry.coins_country += 50;
+        }
+    }
+    //Armia
+    public void ByeSoldier()
+    {
+        if (parametrsCountry.soldier >= 1)
+        {
+            parametrsCountry.soldier -= 1;
+            parametrsCountry.coins_country += 70;
+        }
+    }
+    public void ByeWarMachine()
+    {
+        if (parametrsCountry.war_machine >= 1)
+        {
+            parametrsCountry.war_machine -= 1;
+            parametrsCountry.coins_country += 70;
+        }
+    }
+    public void ByePropaganda()
+    {
+        if (parametrsCountry.propaganda >= 1)
+        {
+            parametrsCountry.propaganda -= 1;
+            parametrsCountry.coins_country += 70;
+        }
+    }
+    //Loilnost and Prestig
+    public void ByeLoailnost()
+    {
+        if (parametrsCountry.loyalty >= 2)
+        {
+            parametrsCountry.loyalty -= 2;
+            parametrsCountry.coins_country += 175;
+        }
+    }
+    public void ByePrestig()
+    {
+        if (parametrsCountry.prestig >= 2)
+        {
+            parametrsCountry.prestig -= 2;
+            parametrsCountry.coins_country += 175;
+        }
+    }
+}
